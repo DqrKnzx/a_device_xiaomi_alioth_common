582a610c5469a9e47b065603dd493fc273417b81
DqrKnzx <DavidYonatan9@hotmail.com>
2023-05-15 21:30:34 -0600

    sm8250-common: Adapt for SkylineUI

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,5 +1,6 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2023 The LineageOS Project
+# Copyright (C) 2022-2023 SkylineUI
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -221,7 +222,7 @@ endif
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/aosp/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += nfc
