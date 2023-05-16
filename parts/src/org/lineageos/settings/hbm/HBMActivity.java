theangelofdead01/void_device_xiaomi_sm8250-common

sm8250-common: parts: Implement HBM & Auto HBM
Co-authored-by: Max Weninger <max.weninger@gmail.com>
Co-authored-by: AnierinB <anierin@evolution-x.org>
Signed-off-by: AnierinB <anierin@evolution-x.org>
Signed-off-by: DarkAngelGR <asavvo01@gmail.com>

parts/src/org/lineageos/settings/hbm/HBMActivity.java
@@ -1,0 +1,46 @@
+/*
+* Copyright (C) 2017 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.lineageos.settings.hbm;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class HBMActivity extends CollapsingToolbarBaseActivity {
+
+    private HBMFragment mHBMFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.content_frame);
+        if (fragment == null) {
+            mHBMFragment = new HBMFragment();
+            getFragmentManager().beginTransaction()
+                .add(R.id.content_frame, mHBMFragment)
+                .commit();
+        } else {
+            mHBMFragment = (HBMFragment) fragment;
+        }
+    }
+}

DqrKnzx/a_device_xiaomi_alioth_common

Signed-off-by: EmanuelCN <emanuelgithub@gmail.com>
Co-Authored-By: Max Weninger <max.weninger@gmail.com>
Co-Authored-By: AnierinB <anierin@evolution-x.org>
Co-Authored-By: EmanuelCN <emanuelgithub@gmail.com>
Co-Authored-By: Yohan Black <deibynovoa3@gmail.com>

parts/src/org/lineageos/settings/hbm/HBMActivity.java
@@ -1,0 +1,46 @@
+/*
+* Copyright (C) 2017 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.lineageos.settings.hbm;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.widget.R;
+
+public class HBMActivity extends CollapsingToolbarBaseActivity {
+
+    private HBMFragment mHBMFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.content_frame);
+        if (fragment == null) {
+            mHBMFragment = new HBMFragment();
+            getFragmentManager().beginTransaction()
+                .add(R.id.content_frame, mHBMFragment)
+                .commit();
+        } else {
+            mHBMFragment = (HBMFragment) fragment;
+        }
+    }
+}